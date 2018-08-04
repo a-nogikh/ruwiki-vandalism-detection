311634dbed13e63b43aaa6c93ac4333610c987b7
Aleksandr Nogikh <wp32pw@gmail.com>
2018-08-04 16:59:58 +0300

    gen_raw_dataset: almost completed implementation

--- cli_utils/gen_raw_dataset.py
+++ cli_utils/gen_raw_dataset.py
@@ -1,8 +1,22 @@
 import click
+import itertools
+import logging
 import random
-from mediawiki import MediaWikiIntegration, MediaWikiApi
+import time
+from dependencies import injector
 from persistence.instance_collection import InstanceCollection
+from models import Instance
+from mediawiki import (MediaWikiIntegration,
+                       MediaWikiApi,
+                       BadRevisionIdException,
+                       MediaWikiApiResourceInaccessible)
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logging.basicConfig()
+
+# Unique random number generator
 def random_id_generator(exclude_ids: set,
                         rev_start: int,
                         rev_end: int):
@@ -19,37 +33,66 @@ def random_id_generator(exclude_ids: set,
         pushed.add(x)
         yield x
 
+
+def instance_generator(id_generator, api_integration: MediaWikiIntegration):
+    rev_id = next(id_generator)
+    while True:
+        try:
+            instance = api_integration.load_single_revision_instance(rev_id)
+            if instance:
+                logger.info("revision {} successfully retrieved".format(rev_id))                
+                yield instance
+                
+        except MediaWikiApiResourceInaccessible:
+            # sleep and retry
+            logger.info("revision {}: couldn't retrieve, sleeping for 5 seconds".format(rev_id))
+            time.sleep(5)
+            continue
         
+        except BadRevisionIdException:
+            logger.info("revision {} does not exist".format(rev_id))
+
+        # selecting next revision id
+        rev_id = next(id_generator)
+
+
 def get_command():
     @click.command()
-    @click.argument('collection', 'collection_name', type=str, help="Collection name")
-    @click.argument('rev_start', type=int, help="# of earliest revision of the range")
-    @click.argument('rev_end', type=int, help="# of newest revision of the range")
-    @click.argument('count', type=int, help="count of revisions to add")
+    @click.option('--collection', 'collection_name', type=str, prompt=True, help="Collection name")
+    @click.option('--rev_start', type=int, prompt=True, help="# of earliest revision of the range")
+    @click.option('--rev_end', type=int, prompt=True, help="# of newest revision of the range")
+    @click.option('--count', type=int, prompt=True, help="count of revisions to add")
     @click.option('--include-bots/--exclude-bots', default=False)
     @click.option('--include-guests/--exclude-guests', default=True)
-    def gen_raw_dataset(collection_name, rev_start, rev_end, count):
-        # TODO: ask for YES/NO!!!
-
-        api = MediaWikiApi()
-        api_integration = MediaWikiIntegration(api)
+    def gen_raw_dataset(collection_name, rev_start, rev_end, count, include_bots, include_guests):
+        api_integration = injector.get(MediaWikiIntegration)
         collection = InstanceCollection(collection_name)
 
         used_revisions = set()
         for x in collection.query_all():
-            used_revisions.insert(x.revision_id)
+            used_revisions.add(x.revision_id)
 
         id_generator = random_id_generator(used_revisions, rev_start, rev_end)
-            
-        found_revisions = 0
-        while found_revisions <= count:
-            rev_id = next(id_generator)
-            instance = api_integration.load_single_revision_instance(rev_id)
+        instance_stream = instance_generator(id_generator, api_integration)
 
-            # process
+        def validate_instance(instance: Instance) -> bool:
+            # TODO implement
+            return True
+        
+        try:
+            saved_items = 0
             
+            filtered_stream = filter(validate_instance, instance_stream)
+            for x in itertools.islice(filtered_stream, count):
+                # load more revisions into the instance!
+                saved_items += 1
+                collection.insert(x)
             
-        # TODO: ask on Ctrl+Z whether  to save
-        pass
+        except KeyboardInterrupt:
+            logger.error("caught keyboard event")
+            pass
+
+        if click.confirm("Do you want to save {} new instances".format(saved_items)):
+            logger.info("Saved")
     
     return gen_raw_dataset
